438d5d8241a853cf0673aa4fb0e3143637bf09be
gtosama <56720244+gtosama@users.noreply.github.com>
2019-10-18 12:46:29 +0200

    Update phone_push.py

--- phone_push.py
+++ phone_push.py
@@ -1,18 +1,18 @@
 from pushbullet import Pushbullet
 import json
 
-Key = 'o.HLjfUoeyHrGtyRRjnCNSZWkUF9Yf8hNy'
+Key = '' #your api key
 
 push = Pushbullet(Key)
 
 print(push.api_key)
 print(push.get_user_id())
 
-phone_id = push.get_device_id('HUAWEI HUAWEI VNS-L31')
+phone_id = push.get_device_id('HUAWEI HUAWEI VNS-L31')#the phone id
 print(phone_id)
 data = {'type' : 'note' ,
         'body' : 'Hello from Micropython',
         'title': 'Alarm 1',
-        'device_iden':phone_id}
+        'device_iden':phone_id} #standard push note
 
-push.make_push(phone_id , json.dumps(data))
+push.make_push(phone_id , json.dumps(data)) # send sms through your phone device
